a411eb20aab4d1823cbc8737921108ffb241014b
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-05-28 21:07:58 +0100

    Use environment port

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
   res.json({hello: 'World!'});
 });
 
-app.listen(3000);
+app.listen(app.get('port'));
