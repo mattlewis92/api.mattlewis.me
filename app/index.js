aaaf410bce33a36c0f83f5884d7eef8907795cd1
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-05-31 17:24:12 +0100

    Refactor middleware to be global and remove email check route

--- app/index.js
+++ app/index.js
@@ -2,10 +2,21 @@ if (process.env.NODE_ENV === 'production') {
   require('newrelic');
 }
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(bodyParser.json());
+
 app.use('/me', require('./routes'));
+
+app.use(function(err, req, res, next) {
+  res.status(400).json({message: err.message});
+});
+
+app.use(function(req, res) {
+  res.status(404).json({message: 'Route not found'});
+});
 
 app.listen(app.get('port'));
--- app/routes.js
+++ app/routes.js
@@ -1,21 +1,9 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var router = express.Router();
 
-router.use(bodyParser.json());
-
-router.get('/email/validate', require('./actions/validateEmail'));
 router.post('/contact', require('./actions/sendEmail'));
 router.get('/social/tweets', require('./actions/getTweets'));
 router.get('/social/github', require('./actions/githubActivity'));
 router.get('/social/linkedin', require('./actions/linkedInProfile'));
-
-router.use(function(req, res) {
-  res.status(404).json({message: 'Route not found'});
-});
-
-router.use(function(err, req, res, next) {
-  res.status(400).json({message: err.message});
-});
 
 module.exports = router;
