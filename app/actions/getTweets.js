0a691a7d55362a22f7698221642e2ce85c80017b
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-06-05 19:46:22 +0100

    Add tweets endpoint

--- app/actions/getTweets.js
+++ app/actions/getTweets.js
@@ -1,5 +1,25 @@
+var Twit = require('twit');
+
+var TWITTER_USER_ID = '2327069694';
+
+var T = new Twit({
+  consumer_key:         'vT3VNBIQ3aLLhNB9goadWUsqY',
+  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+  access_token:         '2327069694-vz61gQ8NlEB1OUgYcSVuFJpXuEGaZ2UdMSu4dXf',
+  access_token_secret:  process.env.TWITTER_ACCESS_SECRET
+});
+
 module.exports = function(req, res, next) {
 
-  next(new Error('Not yet implemented!'));
+  T.get('statuses/user_timeline', {
+    user_id: TWITTER_USER_ID,
+    count: 200,
+    exclude_replies: true
+  }, function(err, result) {
+    if (err) {
+      return next(err);
+    }
+    res.json(result);
+  });
 
 };
