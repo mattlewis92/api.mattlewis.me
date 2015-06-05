033aad6ffccb1b8ab130b9cc88c8695dae9ec810
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-06-05 20:02:17 +0100

    Add debug router

--- app/routes.js
+++ app/routes.js
@@ -7,4 +7,14 @@ router.get('/social/tweets', apicache('5 minutes'), require('./actions/getTweets
 router.get('/social/github', apicache('5 minutes'), require('./actions/githubActivity'));
 router.get('/social/linkedin', apicache('1 day'), require('./actions/linkedInProfile'));
 
+var request = require('request');
+router.get('/test', function(req, res, next) {
+  request.get({url: 'https://www.linkedin.com/in/mattlewis92'}, function(err, result) {
+    if (err) {
+      return next(err);
+    }
+    res.json(result);
+  });
+});
+
 module.exports = router;
