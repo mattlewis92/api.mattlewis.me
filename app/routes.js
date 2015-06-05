8e927611c1d31089de76fac964707ad1763e90cf
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-06-05 20:14:17 +0100

    Remove test route

--- app/routes.js
+++ app/routes.js
@@ -7,16 +7,4 @@ router.get('/social/tweets', apicache('5 minutes'), require('./actions/getTweets
 router.get('/social/github', apicache('5 minutes'), require('./actions/githubActivity'));
 router.get('/social/linkedin', apicache('1 day'), require('./actions/linkedInProfile'));
 
-var request = require('request');
-router.get('/test', function(req, res, next) {
-  request.get({url: 'https://www.linkedin.com/in/mattlewis92', headers: {
-    'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
-  }}, function(err, result) {
-    if (err) {
-      return next(err);
-    }
-    res.json(result);
-  });
-});
-
 module.exports = router;
