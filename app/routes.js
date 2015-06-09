f569bc0f211a534f7d78af2e4444f68a6e65a67e
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-06-09 08:33:12 +0100

    Allow configurable cache expiry times per route

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,17 @@
 const router = require('koa-router')();
 
-var cacheCheck = function* (next) {
-  if (yield* this.cashed()) {
-    return;
+function cacheCheck(expiry) {
+  return function* (next) {
+    if (yield* this.cashed(expiry)) {
+      return;
+    }
+    yield next;
   }
-  yield next;
-};
+}
 
 router.get('/', require('./actions/default'));
-router.get('/social/tweets', cacheCheck, require('./actions/getTweets'));
-router.get('/social/github', cacheCheck, require('./actions/githubActivity'));
+router.get('/social/tweets', cacheCheck(), require('./actions/getTweets'));
+router.get('/social/github', cacheCheck(), require('./actions/githubActivity'));
 router.post('/contact', require('./actions/sendEmail'));
 
 module.exports = router;
