571e0c4f5897fab9f500168811078b2d8d55245b
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-06-09 08:17:34 +0100

    Load heroku config from the shell

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,30 @@
 var gulp   = require('gulp');
 var nodemon   = require('gulp-nodemon');
+var exec = require('child_process').exec;
 
-gulp.task('start', function () {
-  nodemon({
-    script: 'app/index.js',
-    ext: 'js',
-    env: { 'NODE_ENV': 'development' }
+gulp.task('start', function() {
+
+  exec('heroku config', function puts(error, stdout, stderr) {
+
+    var env = {
+      'NODE_ENV': 'development'
+    };
+
+    stdout.trim().split('\n').forEach(function(line, index) {
+      if (index !== 0) {
+        var parts = line.split(':');
+        env[parts[0].trim()] = parts[1].trim();
+      }
+    });
+
+    nodemon({
+      script: 'app/index.js',
+      ext: 'js',
+      env: env
+    });
+
   });
+
 });
 
 gulp.task('default', ['start']);
