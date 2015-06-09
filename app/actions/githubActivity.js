91eb482324ca13df014dbc72ec523abc251270f9
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-06-09 18:53:14 +0100

    Use fetch instead of request

--- app/actions/githubActivity.js
+++ app/actions/githubActivity.js
@@ -1,22 +1,9 @@
-const request = require('request');
-const bluebird = require('bluebird');
+const fetch = require('node-fetch');
 const GITHUB_USERNAME = 'mattlewis92';
-bluebird.promisifyAll(request);
 
 module.exports = function* () {
 
-  const result = yield request.getAsync({
-    url: 'https://api.github.com/users/' + GITHUB_USERNAME + '/events/public',
-    json: true,
-    headers: {'User-Agent': 'mattlewis92'}
-  });
-
-  const response = result[0], body = result[1];
-
-  if (response.statusCode !== 200) {
-    throw new Error(body);
-  }
-
-  this.body = body;
+  const result = yield fetch('https://api.github.com/users/' + GITHUB_USERNAME + '/events/public');
+  this.body = yield result.json();
 
 };
