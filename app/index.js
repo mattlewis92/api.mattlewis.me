adb831a34867d58c0d5110bec7948612d5e6a424
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-06-28 14:00:03 +0100

    Cleanup app bootstrapping

--- app/index.js
+++ app/index.js
@@ -1,9 +1,8 @@
 const koa = require('koa');
 const bodyParser = require('koa-bodyparser');
-const cors = require('koa-cors');
+const cors = require('kcors');
 const compress = require('koa-compress');
 const helmet = require('koa-helmet');
-const bluebird = require('bluebird');
 const cache = require('memory-cache');
 const cash = require('koa-cash');
 const router = require('./routes');
@@ -12,17 +11,16 @@ const PORT = process.env.PORT || 5000;
 const app = koa();
 app.proxy = true;
 
-app.use(function* (next) {
-  try {
-    yield next;
-  } catch (err) {
-    this.status = err.status || 400;
-    this.body = {message: err.message};
-    this.app.emit('error', err, this);
-  }
-});
-
 app
+  .use(function* (next) { //error handler middlware
+    try {
+      yield next;
+    } catch (err) {
+      this.status = err.status || 400;
+      this.body = {message: err.message};
+      this.app.emit('error', err, this);
+    }
+  })
   .use(compress({
     level: 9
   }))
@@ -34,20 +32,21 @@ app
     get: function(key, maxAge) {
       var item = cache.get(key);
       if (!item || (new Date().getTime() - item.createdAt) > maxAge) {
-        return bluebird.resolve();
+        return Promise.resolve();
       }
-      return bluebird.resolve(item.data);
+      return Promise.resolve(item.data);
     },
     set: function(key, value) {
-      return bluebird.resolve(cache.put(key, {data: value, createdAt: new Date().getTime()}));
+      return Promise.resolve(cache.put(key, {data: value, createdAt: new Date().getTime()}));
     }
   }))
   .use(router.routes())
-  .use(router.allowedMethods());
-
-app.use(function* () {
-  this.status = 404;
-  this.body = {message: 'Route not found'};
-});
+  .use(router.allowedMethods())
+  .use(function* () { //default route
+    this.status = 404;
+    this.body = {message: 'Route not found'};
+  });
 
 app.listen(PORT);
+
+console.log('App listening on http://127.0.0.1:%d', PORT);
