cbb05e29b595f2c67f9f15a33e98b60e3367b09d
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-09-19 12:00:31 +0100

    Hack slack post derps to the channel

--- app/actions/slack/derp.js
+++ app/actions/slack/derp.js
@@ -1,6 +1,18 @@
+import Slack from 'node-slack';
+const slack = new Slack(process.env.SLACK_WEBHOOK);
+
 export default async function() {
 
-  console.log(this.request.body); //eslint-disable-line no-console
-  this.body = {text: 'Derp'};
+  await slack.send({
+    channel: '#development',
+    username: 'Derp bot',
+    text: 'Herp a derp',
+    attachments: [{
+      fallback: 'Herp a derp',
+      image_url: 'http://i.imgur.com/ecrlmId.jpg'
+    }]
+  });
+
+  this.body = '';
 
 }
