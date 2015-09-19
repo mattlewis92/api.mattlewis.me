596775301454ab4d22de94e54f1524a2374d49f6
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-09-19 12:09:28 +0100

    Choose a random derp image from imgur

--- app/actions/slack/derp.js
+++ app/actions/slack/derp.js
@@ -1,7 +1,19 @@
 import Slack from 'node-slack';
+import fetch from 'node-fetch';
 const slack = new Slack(process.env.SLACK_WEBHOOK);
+const IMGUR_CLIENT_ID = 'de44c1f57f60e41';
 
 export default async function() {
+
+  const result = await fetch('https://api.imgur.com/3/gallery/r/derp/0', {
+    headers: {
+      Authorization: 'Client-ID ' + IMGUR_CLIENT_ID
+    }
+  });
+
+  const images = await result.json();
+
+  var item = images.data[Math.floor(Math.random() * images.data.length)];
 
   await slack.send({
     channel: '#development',
@@ -9,7 +21,7 @@ export default async function() {
     text: 'Herp a derp',
     attachments: [{
       fallback: 'Herp a derp',
-      image_url: 'http://i.imgur.com/ecrlmId.jpg'
+      image_url: item.link
     }]
   });
 
