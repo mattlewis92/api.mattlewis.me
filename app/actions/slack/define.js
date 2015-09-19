7ea321970181b4f106b5b3006afc4d46a2729b39
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-09-19 12:37:58 +0100

    Add urban dictionary command

---
+++ app/actions/slack/define.js
@@ -0,0 +1,29 @@
+import fetch from 'node-fetch';
+import Slack from 'node-slack';
+const slack = new Slack(process.env.SLACK_WEBHOOK);
+
+export default async function() {
+
+  const result = await fetch(`http://api.urbandictionary.com/v0/define?term=${this.request.body.text}`);
+  const resultBody = await result.json();
+
+  resultBody.list.sort(function(a, b) {
+    return b.thumbs_up - a.thumbs_up;
+  });
+
+  const topResult = resultBody.list[0];
+
+  await slack.send({
+    channel: `#${this.request.body.channel_name}`,
+    username: 'Urban dictionary',
+    icon_url: 'http://i.imgur.com/wrd6aDd.png',
+    text: `*${this.request.body.text}*: ${topResult.definition}`,
+    attachments: [{
+      fallback: topResult.definition,
+      text: `Example: ${topResult.example}`
+    }]
+  });
+
+  this.body = '';
+
+}
--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,7 @@ import tweetsAction from './actions/social/getTweets';
 import githubAction from './actions/social/githubActivity';
 import sendEmailAction from './actions/contact/sendEmail';
 import slackDerpAction from './actions/slack/derp';
+import slackDefineAction from './actions/slack/define';
 
 const cacheCheck = function(expiry) {
   return function* (next) {
@@ -20,7 +21,8 @@ const routes = [
   route.get('/social/tweets', compose([cacheCheck(), tweetsAction])),
   route.get('/social/github', compose([cacheCheck(), githubAction])),
   route.post('/contact', compose([sendEmailAction])),
-  route.post('/slack/derp', compose([slackDerpAction]))
+  route.post('/slack/derp', compose([slackDerpAction])),
+  route.post('/slack/define', compose([slackDefineAction]))
 ];
 
 export default compose(routes);
