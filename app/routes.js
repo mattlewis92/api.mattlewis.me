00dbd48bb8caafd367472ee17781b962783c4a82
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-09-20 12:27:34 +0100

    Return better error message

--- app/routes.js
+++ app/routes.js
@@ -19,7 +19,10 @@ const cacheCheck = function(expiry) {
 
 const limitMiddleware = limit({
   duration: 1000 * 60 * 10,
-  max: 1
+  max: 1,
+  accessLimited: {
+    message: `Sorry but you can't send that many emails`
+  }
 });
 
 const routes = [
