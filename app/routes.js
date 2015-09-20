446a99d7149f5d1d371981cd3c8d38e29009487e
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-09-20 12:22:43 +0100

    Add rate limiter to contact endpoint

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 import route from 'koa-route';
 import compose from 'composition';
+import limit from 'koa-better-ratelimit';
 import defaultAction from './actions/default';
 import tweetsAction from './actions/social/getTweets';
 import githubAction from './actions/social/githubActivity';
@@ -16,11 +17,16 @@ const cacheCheck = function(expiry) {
   };
 };
 
+const limitMiddleware = limit({
+  duration: 1000 * 60 * 10,
+  max: 1
+});
+
 const routes = [
   route.get('/', compose([defaultAction])),
   route.get('/social/tweets', compose([cacheCheck(), tweetsAction])),
   route.get('/social/github', compose([cacheCheck(), githubAction])),
-  route.post('/contact', compose([sendEmailAction])),
+  route.post('/contact', compose([limitMiddleware, sendEmailAction])),
   route.post('/slack/derp', compose([slackDerpAction])),
   route.post('/slack/define', compose([slackDefineAction]))
 ];
