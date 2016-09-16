191cb08734f295356bb1807fc61c82b67792cb7b
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-09-16 14:15:37 +0100

    Only accept numeric ticket ids

--- app/actions/slack/zendeskTicketNumber.js
+++ app/actions/slack/zendeskTicketNumber.js
@@ -3,14 +3,20 @@ const slack = new Slack(process.env.SLACK_WEBHOOK);
 
 export default function* () {
 
-  const ticketId = this.request.body.text;
-
-  yield slack.send({
+  const ticketId = this.request.body.text.replace('#', '').trim();
+  const message = {
     channel: `#${this.request.body.channel_name}`,
     username: 'Zendesk ticket link',
     icon_emoji: ':snorlax:',
     text: `https://socialsignin.zendesk.com/agent/tickets/${ticketId}`
-  });
+  };
+
+  if (!ticketId.match(/^\d+$/)) {
+    message.icon_emoji = ':middle_finger';
+    message.text = 'Ticket ids can only be numeric numbers';
+  }
+
+  yield slack.send(message);
 
   this.body = '';
 
